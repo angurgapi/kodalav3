937417d44170d9fde0bf7dac8f50e85c327bde93
angurgapi <sophiekovalsky@gmail.com>
2023-07-25 14:38:45 +0400

    lessonApi modify

--- src/api/lessonApi.ts
+++ src/api/lessonApi.ts
@@ -1,22 +1,22 @@
 import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_API_URL;
+// const apiUrl = import.meta.env.VITE_API_URL;
 
 const lessonApi = axios.create({
   // baseURL: "/api",
-  baseURL: "",
+  baseURL: "/api",
 });
 
 lessonApi.defaults.headers.common["Content-Type"] = "application/json";
 
 export const getLessonData = async (orderNum: string) => {
   console.log("getLessonData triggered", orderNum);
-  console.log("api url:", apiUrl);
-  const response = await lessonApi.get(`/api/lesson/${orderNum}`);
+
+  const response = await lessonApi.get(`/lesson/${orderNum}`);
   return response.data;
 };
 
 export const getAllLessons = async () => {
-  const { data } = await lessonApi.get("/api/lesson");
+  const { data } = await lessonApi.get("lesson");
   return data;
 };
