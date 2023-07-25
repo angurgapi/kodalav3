38b565f59affea4ea49356eb1f31d7a1e6cbb668
angurgapi <sophiekovalsky@gmail.com>
2023-07-25 14:27:48 +0400

    rm base from lesson axios

--- src/api/lessonApi.ts
+++ src/api/lessonApi.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const lessonApi = axios.create({
-  baseURL: "/api",
+  // baseURL: "/api",
+  baseURL: "",
 });
 
 lessonApi.defaults.headers.common["Content-Type"] = "application/json";
@@ -11,11 +12,11 @@ lessonApi.defaults.headers.common["Content-Type"] = "application/json";
 export const getLessonData = async (orderNum: string) => {
   console.log("getLessonData triggered", orderNum);
   console.log("api url:", apiUrl);
-  const response = await lessonApi.get(`lesson/${orderNum}`);
+  const response = await lessonApi.get(`/api/lesson/${orderNum}`);
   return response.data;
 };
 
 export const getAllLessons = async () => {
-  const { data } = await lessonApi.get("lesson");
+  const { data } = await lessonApi.get("/api/lesson");
   return data;
 };
